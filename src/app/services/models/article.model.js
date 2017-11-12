825eb9542bcce06c7a99417bea91521f911b0167
mihail-petrov <mail@mihail-petrov.me>
2017-11-12 23:05:05 +0200

    Add article model abstraction

--- src/app/services/models/article.model.js
+++ src/app/services/models/article.model.js
@@ -2,36 +2,77 @@
 
     var BASE_URL = "article";
 
-
-    var create = function () {
-        // archive api collection
-    };
-
-
-    var read = function () {
-        // archive api collection
-    };
-
-
-    var update = function () {
-        // archive api collection
-    };
-
-
-    var archive = function () {
-        // archive api collection
-    };
-
-
-    var Service = function () {
+    /**
+     * 
+     * @param {*} request 
+     */
+    var create = function (request) {
         return {
-            create: create,
-            read: read,
-            update: update,
-            archive: archive
+            article : function(body) {
+                return request.post([BASE_URL, body]);
+            }
         }
     };
 
+    /**
+     * 
+     * @param {*} request 
+     */
+    var read = function (request) {
+        return {
+            
+            all : function() {
+                return request.get([BASE_URL])
+            },
 
-    angular.module('realworld.models').factory('ArticleModel', Service);
+            feed : function() {
+                return request.get([BASE_URL, 'feed'])
+            },
+
+            byAuthor : function() {
+                return request.get([BASE_URL])
+            },
+
+            byTag : function() {
+                return request.get([BASE_URL])
+            },
+
+            bySlug : function() {
+                return request.get([BASE_URL])
+            },
+
+            byFavorite : function() {
+                return request.get([BASE_URL])
+            }
+        }
+    };
+
+    /**
+     * 
+     * @param {*} request 
+     */
+    var update = function (request) {
+        // update api collection
+    };
+
+    /**
+     * 
+     * @param {*} request 
+     */
+    var archive = function (request) {
+        // archive api collection
+    };
+
+
+    var Service = function (Request) {
+        return {
+            create  : create(Request),
+            read    : read(Request),
+            update  : update(Request),
+            archive : archive(Request)
+        }
+    };
+
+    Service.$injector = ['Request'];
+    angular.module('realworld.model').factory('ArticleModel', Service);
 })()
