b1144a95a18cd31a51bb27ab2213b9e733497131
mihail-petrov <mail@mihail-petrov.me>
2017-11-12 16:26:57 +0200

    Add base models nesesary for communication with external API
    The models are separated by there features (based on the example POSTMAN
    collection)
    
    - article : resources related to the article
    - auth : resources for user authentication
    - comments : resources for dealing with comment collection
    - profile : resources for managing user information and settings
    - tag : resources for managing tag collection

---
+++ src/app/services/models/profile.model.js
@@ -0,0 +1,37 @@
+(function () {
+
+    var BASE_URL = "profile";
+
+
+    var create = function () {
+        // archive api collection
+    };
+
+
+    var read = function () {
+        // archive api collection
+    };
+
+
+    var update = function () {
+        // archive api collection
+    };
+
+
+    var archive = function () {
+        // archive api collection
+    };
+
+
+    var Service = function () {
+        return {
+            create: create,
+            read: read,
+            update: update,
+            archive: archive
+        }
+    };
+
+
+    angular.module('realworld.models').factory('ProfileModel', Service);
+})()
