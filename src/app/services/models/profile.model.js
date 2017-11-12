9ed75a43511eccf1bdef1345e7230b9e6497c0e7
mihail-petrov <mail@mihail-petrov.me>
2017-11-12 23:07:26 +0200

    Add profile model abstraction

--- src/app/services/models/profile.model.js
+++ src/app/services/models/profile.model.js
@@ -2,36 +2,65 @@
 
     var BASE_URL = "profile";
 
-
-    var create = function () {
-        // archive api collection
-    };
-
-
-    var read = function () {
-        // archive api collection
-    };
-
-
-    var update = function () {
-        // archive api collection
-    };
-
-
-    var archive = function () {
-        // archive api collection
-    };
-
-
-    var Service = function () {
+    /**
+     * 
+     * @param {*} request 
+     */
+    var create = function (request) {
         return {
-            create: create,
-            read: read,
-            update: update,
-            archive: archive
+            follow : function(_user, _body) {
+                return request.post([BASE_URL, _user, 'follow'], _body)
+            }
         }
     };
 
+    /**
+     * 
+     * @param {*} request 
+     */
+    var read = function (request) {
+        return {
 
-    angular.module('realworld.models').factory('ProfileModel', Service);
+            profile : function() {
+                return request.get([BASE_URL])
+            }
+        }
+    };
+
+    /**
+     * 
+     * @param {*} request 
+     */
+    var update = function (request) {
+        // archive api collection
+    };
+
+
+    /**
+     * 
+     * @param {*} request 
+     */
+    var archive = function (request) {
+        return {
+            unfollow : function(_user) {
+                return request.delete([BASE_URL, _user, 'follow']);
+            }
+        }
+    };
+
+    /**
+     * 
+     * @param {*} Request 
+     */
+    var Service = function (Request) {
+        return {
+            create  : create(Request),
+            read    : read(Request),
+            update  : update(Request),
+            archive : archive(Request)
+        }
+    };
+
+    Service.$injector = ['Request'];
+    angular.module('realworld.model').factory('ProfileModel', Service);
 })()
